92be98fe0434e61e1550e69e5821d0031b76b9f4
Antonio <a.shishkin.b@gmail.com>
2018-09-18 15:48:35 +0300

    tmp

--- kaminari-activerecord/lib/kaminari/activerecord/active_record_model_extension.rb
+++ kaminari-activerecord/lib/kaminari/activerecord/active_record_model_extension.rb
@@ -13,12 +13,12 @@ module Kaminari
       eval <<-RUBY, nil, __FILE__, __LINE__ + 1
         def self.#{Kaminari.config.page_method_name}(num = nil)
           per_page = max_per_page && (default_per_page > max_per_page) ? max_per_page : default_per_page
-          max_id = self.select("{self.table_name}.id AS max_id").order("id desc").limit(1)[0]["max_id"] - (per_page * num.to_i)
+          max_id = self.select("%s.id AS max_id" % self.table_name).order("id desc").limit(1)[0]["max_id"] - (per_page * num.to_i)
 
           min_id = (per_page * 2) * num.to_i #
 
 
-          limit(per_page).where("#{self.table_name}.id BETWEEN ? AND ?", max_id, min_id).extending do
+          limit(per_page).where("%s.id BETWEEN ? AND ?" % self.table_name, max_id, min_id).extending do
             include Kaminari::ActiveRecordRelationMethods
             include Kaminari::PageScopeMethods
           end
